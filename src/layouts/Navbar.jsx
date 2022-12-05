d2110545c9fa0db0e1c6a19cb48cab6ef3a54296
JacquesPoulin <jacques.poulin64@gmail.com>
2022-12-05 23:05:54 +0100

    [NAVBAR] : terminée à 80% il manque le responsive v2

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,30 +1,11 @@
 // --- REACT ---
-import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-
-import { Sling as Hamburger } from "hamburger-react";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 // --- DATAS ---
 import navbarList from "../../data/navbarList";
 
 const navbar = () => {
-  // ! ***** STATES ******
-  // > Handle the navbar background-color on scroll
-  const [isColor, setIsColor] = useState(false);
-
-  // ! ***** FUNCTIONS ******
-  // > Change the navbar background-color when scrolling
-  const changeColor = () => {
-    if (isOpened === false && window.scrollY >= 15) {
-      setIsColor(true);
-    } else {
-      setIsColor(false);
-    }
-  };
-
-  // > To listen the navbar scrolling event
-  window.addEventListener("scroll", changeColor);
-
   // ! ***** RENDERING ******
   return (
     <header>
