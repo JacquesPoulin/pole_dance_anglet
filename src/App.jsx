f27c3f055d57b979fc37cc89cc6c266c4ea4af80
JacquesPoulin <jacques.poulin64@gmail.com>
2022-12-05 19:45:14 +0100

    [ROUTING] : Routes done

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,41 @@
 import "./sass/main.scss";
 
 // REACT
-import React from "react";
-import { useRoutes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 // ALL APP COMPONENTS
-import Footer from "./layouts/Footer";
 import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
+import Homepage from "./pages/Homepage";
+import Biographie from "./pages/Biographie";
+import Evjf from "./pages/Evjf";
+import Galerie from "./pages/Galerie";
+import Pilates from "./pages/Pilates";
+import PoleDance from "./pages/PoleDance";
+import Tarifs from "./pages/Tarifs";
+import Yoga from "./pages/Yoga";
 
-// APP ROUTES
-import routesConfig from "./routes";
-
-function App() {
-  const routes = useRoutes(routesConfig);
+const App = () => {
   return (
-    <div>
-      <Navbar />
-      {routes}
-      <Footer />
+    <div className="App">
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="*" element={<Homepage />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/acceuil" element={<Homepage />} />
+          <Route path="/biographie" element={<Biographie />} />
+          <Route path="/evjf" element={<Evjf />} />
+          <Route path="/galerie" element={<Galerie />} />
+          <Route path="/pilates" element={<Pilates />} />
+          <Route path="/poledance" element={<PoleDance />} />
+          <Route path="/tarifs" element={<Tarifs />} />
+          <Route path="/yoga" element={<Yoga />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
     </div>
   );
-}
+};
 
 export default App;
