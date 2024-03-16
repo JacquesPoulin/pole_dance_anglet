7da9eccde0542771569d8b662a08d33711751305
Jacques <jacques.poulin64@gmail.com>
2024-03-16 20:08:20 +0100

    Ajout des configs

---
+++ src/utils/configContact.js
@@ -0,0 +1,9 @@
+// ! configs pour l'envoi de mail via EmailJS
+const configContact = {
+    serviceId: "service_wf7pi5i",
+    templateId: "template_l884zi7",
+    userId: "6khS4jnWpNLhe37q8",
+  };
+  
+  export default configContact;
+  
---
+++ src/utils/configDevis.js
@@ -0,0 +1,14 @@
+// ! configs pour l'envoi de mail via EmailJS
+const configDevis = {
+  serviceId: "service_wf7pi5i",
+  templateId: "template_p9ac5wp",
+  userId: "6khS4jnWpNLhe37q8",
+};
+
+// const configContact = {
+//   serviceId: "service_1u39dwc",
+//   templateId: "template_l884zi7",
+//   userId: "6khS4jnWpNLhe37q8",
+// };
+
+export default configDevis;
