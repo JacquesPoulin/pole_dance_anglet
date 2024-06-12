5a2f0e902018809586b5b778e9ea877fa79dbdef
Jacques <jacques.poulin64@gmail.com>
2024-06-12 20:27:36 +0200

    MAJ de la gallerie photo

--- src/pages/Galerie.jsx
+++ src/pages/Galerie.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 
 // --- DATAS ---
 import galleryList from "../../data/galleryList";
 
 const Galerie = () => {
+  // État pour gérer la visibilité des photos
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
+
+  // Fonction pour basculer l'état de visibilité
+  const toggleShowPhotos = () => {
+    setShowAllPhotos(!showAllPhotos);
+  };
+
+  // Déterminer l'index à partir duquel les photos seront cachées/affichées
+  const halfLength = Math.ceil(galleryList.length / 2.5);
+
   return (
     <div name="Galerie" className="gallerie">
       <div>
@@ -30,16 +41,25 @@ const Galerie = () => {
       {/*** MAIN GALLERY BLOC ***/}
       <div className="gallery">
         {/*** GALLERY COLUMNS ***/}
-        {galleryList?.map(({ id, imgSource, alt, galleyStyle }) => (
-          <div className="gallery__column" key={id}>
-            <div className="gallery__link">
-              {/*** PICTURES ***/}
-              <figure className="gallery__thumb">
-                <img src={imgSource} alt={alt} className={galleyStyle} />
-              </figure>
+        {galleryList
+          .slice(0, showAllPhotos ? galleryList.length : halfLength)
+          .map(({ id, imgSource, alt, galleyStyle }) => (
+            <div className="gallery__column" key={id}>
+              <div className="gallery__link">
+                {/*** PICTURES ***/}
+                <figure className="gallery__thumb">
+                  <img src={imgSource} alt={alt} className={galleyStyle} />
+                </figure>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+      </div>
+
+      {/* BOUTON POUR BASCULER L'AFFICHAGE */}
+      <div className="buttonContainer">
+        <button className="toggleButton" onClick={toggleShowPhotos}>
+          {showAllPhotos ? "Voir moins" : "Voir plus"}
+        </button>
       </div>
     </div>
   );
