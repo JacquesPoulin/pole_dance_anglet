4a7fcec9e456d5dde2d7fc37d66926e9ddddc35e
jacques <jacques.poulin64@gmail.com>
2026-03-10 20:05:26 +0100

    Ajout de toutes les favicons et manifest

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -4,6 +4,6 @@ export const apiVersion =
 	process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-08';
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 export const projectId =
-	process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'votre_project_id';
-// Pour info, je laisse 'votre_project_id' par défaut pour ne pas bloquer le build si vous n'avez pas encore votre compte.
+	process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'fufb4bda'; // Remplacé par votre vrai projectId
+// J'ai mis votre vrai projectId au lieu de 'votre_project_id' pour éviter l'erreur de build.
 export const useCdn = false;
